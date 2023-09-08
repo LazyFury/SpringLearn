2132f2bc1bf37dea84aa9806d5341841db76fc18
suke <suke971219@gmail.com>
2023-09-08 15:11:24 +0800

    Changes

--- src/main/java/io/lazyfury/mall/code/api/UploadApi.java
+++ src/main/java/io/lazyfury/mall/code/api/UploadApi.java
@@ -1,6 +1,7 @@
 package io.lazyfury.mall.code.api;
 
 
+import jakarta.annotation.Nonnull;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,14 +33,34 @@ public class UploadApi {
     }
 
     @PostMapping("/upload")
-    public HashMap<String, Object> upload(@RequestPart MultipartFile file) throws IOException {
+    public HashMap<String, Object> upload(@Nonnull @RequestPart MultipartFile file) throws IOException {
+        //排除未知文件后缀和可执行文件
+        val ext = Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf(".") + 1);
+        if (ext.equals("exe") || ext.equals("bat") || ext.equals("sh") || ext.equals("cmd")) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("code", 400);
+            map.put("msg", "上传失败");
+            map.put("data", "不支持的文件类型");
+            return map;
+        }
+        //仅支持视频 图片 音频类型 使用content type判断
+        val contentType = file.getContentType();
+        if (contentType == null || (!contentType.startsWith("image") && !contentType.startsWith("video") && !contentType.startsWith("audio"))) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("code", 400);
+            map.put("msg", "上传失败");
+            map.put("data", "不支持的文件类型");
+            return map;
+        }
+
+
         val f = new File(upload.getAbsolutePath(), Objects.requireNonNull(file.getOriginalFilename()));
         System.out.println(f.getAbsoluteFile());
         file.transferTo(f.getAbsoluteFile());
         HashMap<String, Object> map = new HashMap<>();
         map.put("code", 200);
         map.put("msg", "上传成功");
-        map.put("data", "http://127.0.0.1:8080/static/upload/" + file.getOriginalFilename());
+        map.put("data", "http://127.0.0.1:13587/static/upload/" + file.getOriginalFilename());
         return map;
     }
 }
