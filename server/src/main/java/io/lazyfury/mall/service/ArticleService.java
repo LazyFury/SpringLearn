d5a149a849b9badc09637738d7ffaf69877404eb
suke <suke971219@gmail.com>
2023-09-17 15:10:27 +0800

    move files to server

--- server/src/main/java/io/lazyfury/mall/service/ArticleService.java
+++ server/src/main/java/io/lazyfury/mall/service/ArticleService.java
@@ -9,11 +9,9 @@ import io.lazyfury.mall.repository.ArticleViewLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,32 +33,52 @@ public class ArticleService {
      * Archive year month day
      *
      * @return List<HashMap < String, Object>>
+     * {
+     * "date": "2020",
+     * "months": [
+     * date: "2020-01",
+     * days: [
+     * date: "2020-01-01",
+     * count: 1,
+     * articles: []
+     * ]
+     * ]
+     * }
      */
     public List<HashMap<String, Object>> archiveYearMonthAndDay() {
         List<Article> articles = (List<Article>) articleRepository.findAll();
+
+        var sortKeyByLocalDate = Comparator.comparing(LocalDate::toString).reversed();
+
         var yearGroupArticles = articles.stream().collect(Collectors.groupingBy(v -> v.getCreated().getYear(), Collectors.toList()));
+        var yearGroupKeys = yearGroupArticles.keySet().stream().sorted(Comparator.reverseOrder()).toList();
         List<HashMap<String, Object>> years = new ArrayList<>();
-        for (var entry : yearGroupArticles.entrySet()) {
+        for (var key : yearGroupKeys) {
+            var entry = yearGroupArticles.get(key);
             List<HashMap<String, Object>> months = new ArrayList<>();
-            var monthGroupArticles = entry.getValue().stream().collect(Collectors.groupingBy(v -> v.getCreated().toLocalDate().format(DateTimeFormatter.ofPattern("yyyy-MM")), Collectors.toList()));
-            for (var monthEntry : monthGroupArticles.entrySet()) {
+            var monthGroupArticles = entry.stream().collect(Collectors.groupingBy(v -> v.getCreated().toLocalDate().format(DateTimeFormatter.ofPattern("yyyy-MM")), Collectors.toList()));
+            var monthGroupKeys = monthGroupArticles.keySet().stream().sorted(Comparator.comparing(String::toString).reversed()).toList();
+            for (var monthKey : monthGroupKeys) {
+                var monthEntry = monthGroupArticles.get(monthKey);
                 List<HashMap<String, Object>> days = new ArrayList<>();
-                var dayGroupArticles = monthEntry.getValue().stream().collect(Collectors.groupingBy(v -> v.getCreated().toLocalDate(), Collectors.toList()));
-                for (var dayEntry : dayGroupArticles.entrySet()) {
+                var dayGroupArticles = monthEntry.stream().collect(Collectors.groupingBy(v -> v.getCreated().toLocalDate(), Collectors.toList()));
+                List<LocalDate> dayGroupKeys = dayGroupArticles.keySet().stream().sorted(sortKeyByLocalDate).toList();
+                for (var dayKey : dayGroupKeys) {
+                    var dayEntry = dayGroupArticles.get(dayKey);
                     var map = new HashMap<String, Object>();
-                    map.put("date", dayEntry.getKey());
-                    map.put("count", dayEntry.getValue().size());
-                    map.put("articles", dayEntry.getValue());
+                    map.put("date", dayKey);
+                    map.put("count", dayEntry.size());
+                    map.put("articles", dayEntry);
                     days.add(map);
                 }
                 var map = new HashMap<String, Object>();
-                map.put("date", monthEntry.getKey());
+                map.put("date", monthKey);
                 map.put("days", days);
                 map.put("count", days.size());
                 months.add(map);
             }
             var map = new HashMap<String, Object>();
-            map.put("date", entry.getKey());
+            map.put("date", key);
             map.put("months", months);
             map.put("count", months.size());
             years.add(map);
